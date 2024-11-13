c919079ad8be41e748c6f32c58cab2547a0e5816
wbridgri <will.bridges@comcast.net>
2024-11-13 09:24:55 -0600

    helllp!

--- js/app.js
+++ js/app.js
@@ -16,6 +16,48 @@ let myName = document.getElementById('myName')
 
 myName.innerText = 'Will'
 
-console.log(myName)
+// console.log(myName)
 
+let hero = 'Superman';
+let alias = 'Clark Kent';
+let powers = ['super strength', 'x-ray vision', 'super speed', 'heat vision']
+let enemies = ['Lex Luthor', 'Doomsay', 'Darkseid']
+
+const heroName = document.getElementById('heroName');
+heroName.innerText = hero;
+
+
+
+const aliasName = document.getElementById('aliasName');
+aliasName.innerText = alias;
+
+const powerList = document.getElementById('powerList');
+const enemyList = document.getElementById('enemyList');
+
+//loop through array and create an li for each element, then append to powerList
+powers.forEach(power => {
+    const li = document.createElement('li');
+    // console.log(li);
+    li.innerText = power;
+    powerList.appendChild(li)
+    
+
+})
+
+enemies.forEach(enemy => {
+    const li = document.createElement('li');
+    li.innerText = enemy;
+    enemyList.appendChild(li);
+})
+
+
+/**
+ * Refactor as a Class-based object
+ * 
+ * 
+ * class Superhero {
+ * }
+ */
+
+class Superhero
 
